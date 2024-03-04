0795f274ca1d8ec2e6fb4dcbd5c3222f0c5482cc
kaungpyaesoneaunggic <kaungpyaesoneaung@gicjp.com>
2024-03-04 13:15:01 +0630

    Type Basics

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+let pname = 'Kyaw Kyaw';
+let number = 21;
+let single = true;
+let students = [
+    'Kyaw Kyaw',
+    'Naing Naing',
+];
+students.push('Soe Soe');
+console.log(students);
+let studentsInfo = {
+    fname: 'Kyaw Kyaw',
+    age: 18,
+    married: false
+};
+studentsInfo.age = 'lunch';
+console.log(studentsInfo);
---
+++ index.ts
@@ -0,0 +1,1 @@
+let pname:string= 'Kyaw Kyaw';
