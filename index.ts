538f82836e73cd5a7b3f9f144d304de584052d32
kaungpyaesoneaunggic <kaungpyaesoneaung@gicjp.com>
2024-03-04 13:15:53 +0630

    #3 Type Basics

--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
-let pname:string= 'Kyaw Kyaw';
+let fname:string;
+let age:number;
+let isSingle:boolean;
+
+fname='Kaung Pyae';
+age=22;
+isSingle=true;
+
+
+let planet = 'Earth';
+let moons = 1;
+let isLarge = false;
+
+planet='Saturn';
+moons = 145;
+isLarge=true;
+
+
+//null & undefinend;
+// loced in and will never be changed
+let nullItem:null;
+let undefinendItem:undefined;
+
+nullItem=null;
+undefinendItem=undefined;
